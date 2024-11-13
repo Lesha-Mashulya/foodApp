acc5c746309314d9e9578be6f98e0b51a41eb16d
licnyj <oljxa.ru@gmail.com>
2024-11-13 21:12:56 +0300

    v1

--- app/src/main/java/com/example/foodfocus/StartActivity.kt
+++ app/src/main/java/com/example/foodfocus/StartActivity.kt
@@ -1,20 +1,25 @@
 package com.example.foodfocus
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodfocus.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.nextButton.setOnClickListener { it: View ->
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
